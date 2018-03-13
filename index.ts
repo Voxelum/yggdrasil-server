8361dd6470061d2859a49bedd4fc209023f84c6d
ci010 <cijhn@hotmail.com>
2018-03-12 23:21:24 -0700

    handle all request

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express();
+
+app.post('/authenticate', (req, resp) => {
+
+})
+
+app.post('/refresh', (req, resp) => {
+    
+})
+
+app.post('/validate', (req, resp) => {
+
+})
+
+app.post('/signout', (req, resp) => {
+
+})
+
+
+app.post('/invalidate', (req, resp) => {
+
+})
+
+app.listen(8080);
